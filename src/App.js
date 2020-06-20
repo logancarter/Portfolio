2a44dff9b8964f3aafae02299efc124947167932
Logan Carter <logmac13@gmail.com>
2020-06-19 23:27:18 -0700

    Added portfolio text

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hi world
+        Portfolio site coming soon
       </header>
     </div>
   );
