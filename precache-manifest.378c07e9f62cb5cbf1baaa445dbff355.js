d455e56b7a868244df7d60b93a998b92715f1ac5
Logan Carter <logmac13@gmail.com>
2020-06-19 23:02:44 -0700

    Updates

---
+++ precache-manifest.378c07e9f62cb5cbf1baaa445dbff355.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e466dbb12b2688863f6e69093cc1e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4da272966316fc6d099b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4da272966316fc6d099b",
+    "url": "/static/js/main.3f64975c.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
