111bb1ef632bfc88fd10a519297a3aa86492e983
Logan Carter <logmac13@gmail.com>
2020-06-21 22:41:22 -0700

    Add basic routing

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,27 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
 import './Portfolio.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function Portfolio() {
+export default function Portfolio() {
   return (
-    <div className="Portfolio">
-      <header className="Portfolio-header">
-        <span className="my-name">Logan Carter</span>
-        <div className="navigation">
-          <span id="portfolio">Portfolio</span>
-          <span id="about">About</span>
-        </div>
-      </header>
-
-      <div className="hero-text">
-        <div className="actual-text">
-          <div>UX Designer </div>
-          <div>with a background </div>
-          <div>in Software Engineering</div>
-        </div>
+    <Router>
+      <div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 }
 
-export default Portfolio;
+function Home() {
+  return (
+    <div className="Portfolio">
+    <header className="Portfolio-header">
+      <span className="my-name">Logan Carter</span>
+      <nav className="navigation">
+        <Link to="/" id="portfolio">Portfolio</Link>
+        <Link to="/about" id="about">About</Link>
+      </nav>
+    </header>
+
+    <div className="hero-text">
+      <div className="actual-text">
+        <div>UX Designer </div>
+        <div>with a background </div>
+        <div>in Software Engineering</div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+function About() {
+  return <h2>About</h2>;
+}
